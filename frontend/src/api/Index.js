c7b54561d16fd3dece207500de2c0133433898d7
Armaan Agrawal <armaan@Armaans-MacBook-Air.local>
2023-05-30 16:05:20 +0530

    url bug fixing done

--- frontend/src/api/Index.js
+++ frontend/src/api/Index.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000";
+// const BASE_URL = "http://localhost:4000";
 
 export async function fetchTaskList() {
-  const res = await axios.get(`${BASE_URL}/api/todo-app`);
+  const res = await axios.get(`/api/todo-app`);
   return res.data;
 }
 export async function addTask({ title, completed }) {
-  const res = await axios.post(`${BASE_URL}/api/todo-app`, {
+  const res = await axios.post(`/api/todo-app`, {
     title,
     completed,
   });
   return res.data;
 }
 export async function updateTask({ id, title, completed }) {
-  const res = await axios.put(`${BASE_URL}/api/todo-app`, {
+  const res = await axios.put(`/api/todo-app`, {
     id,
     title,
     completed,
@@ -22,6 +22,6 @@ export async function updateTask({ id, title, completed }) {
   return res.data;
 }
 export async function removeTask({ id }) {
-  const res = await axios.delete(`${BASE_URL}/api/todo-app/${id}`);
+  const res = await axios.delete(`/api/todo-app/${id}`);
   return res.data;
 }
